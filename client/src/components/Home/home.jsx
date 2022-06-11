c89d9802ee5bc05131efa1d7fc5dbb34aa98eb44
ArunKr <arunaj9891@gmail.com>
2022-06-11 09:21:09 +0530

    initial setup

---
+++ client/src/components/Home/home.jsx
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react'
+
+const Home = () => {
+
+    return (
+       <div>
+        <p>
+            Paragraph
+        </p>
+        <span>
+            Home
+        </span>
+        <span>
+            Body
+        </span>
+        <span>
+            Footer
+        </span>
+       </div>
+    )
+}
+
+export default Home
