1699dc2501c7a353d1b64e43dc329748f715126c
gajendra singh <gajjubana92@gmail.com>
2022-06-19 17:24:52 +0530

    fix:login and Register page UI

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,10 +1,29 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import './login.css'
+import loginBg from '../../assets/images/login-bg.jpg'
 
 const Login = () => {
 
     return (
         <div>
-            Login
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-8'>
+                        <div className='loginBg'>
+                            <img src={loginBg} alt="" />
+                        </div>
+                    </div>
+                    <div className='col-4'>
+                        <form className='loginFrom'>
+                            <h3>Account Login</h3>
+                            <input type="text" placeholder="username"></input>
+                            <input type="password" placeholder="Password"></input>
+                            <button type='button'> Submit</button>
+                            <div className="sign-in-account"><span>Don't have an account?</span><a href='/admin/register'>Sign Up</a></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/Login/register.jsx
+++ client/src/components/Login/register.jsx
@@ -1,10 +1,29 @@
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from 'react'
+import './login.css'
+import loginBg from '../../assets/images/login-bg.jpg'
 const Register = () => {
 
     return (
         <div>
-            Register
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-8'>
+                        <div className='loginBg'>
+                            <img src={loginBg} alt="" />
+                        </div>
+                    </div>
+                    <div className='col-4'>
+                        <form className='loginFrom'>
+                            <h3>Account Register</h3>
+                            <input type="text" placeholder="Email Address"></input>
+                            <input type="text" placeholder="username"></input>
+                            <input type="password" placeholder="Password"></input>
+                            <button type='button'> Submit</button>
+                            <div className="sign-in-account"><span>Don't have an account?</span><a href='/admin/login'>Sign In</a></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
