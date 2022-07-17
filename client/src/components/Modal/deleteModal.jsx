045ae9ed956197b7677030ae89cd79f073752132
gajendra singh <gajjubana92@gmail.com>
2022-07-17 21:24:58 +0530

    modal UI fixed

--- client/src/components/Modal/deleteModal.jsx
+++ client/src/components/Modal/deleteModal.jsx
@@ -10,14 +10,15 @@ const DeleteModal = (props) => {
   return (
     <Modal
       show={props.open}
-      onHide={props.handleClose}
+      onHide={props.handleClose}      
+      className="products-page Delete-Product"
     >
       <Modal.Header closeButton>
         <Modal.Title>{props?.title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        Are you sure you want to delete {props?.product?.name}?
       </Modal.Body>
-        Are you sure you want to delete {props?.product?.name}?  
       <Modal.Footer>
         <Button variant="secondary" onClick={props?.onSubmit}>Yes</Button>
         <Button variant="primary" onClick={props?.handleClose}>No</Button>
