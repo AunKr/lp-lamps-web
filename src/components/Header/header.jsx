9c25a4a4cb76f6ddbc1ca8d429f275629099c7ab
ArunKr <arunaj9891@gmail.com>
2022-08-20 21:29:09 +0530

    feat: added side bar open functionality

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 import LogoImage from '../../assets/images/logo.png'
 
 const Header = () => {
+    const [sideBar, setSideBar] = useState(false)
+    const openSideBar = () => {
+        setSideBar(true)
+    }
+    const closeSideBar = () => {
+        setSideBar(false)
+    }
     return (
         <div className='header-area header-sticky animate__animated animate__bounceInDown' data-wow-duration="0.75s" data-wow-delay="0s">
             <div className="container">
@@ -13,14 +20,15 @@ const Header = () => {
                             <a href="/" className="logo">
                                 <img src={LogoImage} alt=""/>
                             </a>
-                            <ul className="nav">
+                            <ul className={`nav ${sideBar ? 'openSideBar' : ''}`} >
+                                <li><a onClick={closeSideBar}>X</a></li>
                                 <li className="scroll-to-section"><a href="/#top" className="active">Home</a></li>
                                 <li className="scroll-to-section"><a href="/#about" className="">About Us</a></li>
                                 <li className="scroll-to-section hoverNav"><a href="/#portfolio">Products</a></li>
                                 <li className="scroll-to-section"><a href="/#blog">Blog</a></li>
                                 <li className="scroll-to-section"><div className="main-red-button"><a href="/#contact">Contact Us</a></div></li>
                             </ul>
-                            <a className="menu-trigger">
+                            <a className="menu-trigger" onClick={openSideBar}>
                                 <span>Menu</span>
                             </a>
                         </nav>
