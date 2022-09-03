65dd68df19ac7b7520fabf9a686b123e973ff9d5
ArunKr <arunaj9891@gmail.com>
2022-09-03 23:33:10 +0530

    fix: footer map address

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -36,16 +36,15 @@ const Footer = () => {
           </div>
           <div class="col-lg-6 col-sm-6 iframeMap">
             <h5>Office Loction</h5>
-            <iframe
-              src="https://www.google.com/maps/place/Alwar+Lamp/@27.0038976,75.7852975,17z/data=!4m12!1m6!3m5!1s0x396db20c7109eaf1:0xd0dc903a8d640291!2sAlwar+Lamp!8m2!3d27.0038853!4d75.7874871!3m4!1s0x396db20c7109eaf1:0xd0dc903a8d640291!8m2!3d27.0038853!4d75.7874871!5m1!1e1"
-              width="600"
-              height="450"
-              frameBorder="0"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-            />
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3555.1319213870183!2d75.77161541436773!3d26.99437636306968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcbaf55f3b6cc9721!2sAlwar%20Lamps%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1662225040186!5m2!1sen!2sin" 
+              width="600" 
+              height="450" 
+              style={{border: 0}}
+              allowfullscreen="" 
+              loading="lazy" 
+              referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
           </div>
 
         </div>
