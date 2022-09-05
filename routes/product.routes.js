19b055c832d1ea5d78d35fb726ecf348ee556df1
ArunKr <arunaj9891@gmail.com>
2022-09-05 18:19:26 +0530

    fix: product list visible to all

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,7 +5,7 @@ const { ensureAuthenticated } = require('../middleware/auth.middleware.js')
 const productRoute = express.Router();
 
 productRoute.post('/create', ensureAuthenticated,controller.upload, controller.productCreate);
-productRoute.get('/get', ensureAuthenticated, controller.getAllProduct);
+productRoute.get('/get', controller.getAllProduct);
 productRoute.patch('/update/:id', ensureAuthenticated , controller.productUpdate);
 productRoute.delete('/delete/:id', ensureAuthenticated , controller.productDelete);
 
