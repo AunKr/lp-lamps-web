dc0234b48929740777b1cc0723ec39fe127d7835
ArunKr <arunaj9891@gmail.com>
2022-10-09 14:32:16 +0530

    fix: removed build server from backend

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ if (cluster.isMaster) {
     console.log("UNHANDLED_REJECTION", error);
   });
 
-  app.use(express.static('client/build'));
-  app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
+  // app.use(express.static('client/build'));
+  // app.get('/*', (req, res) => {
+  // res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  // });
 
   // Start the Server
   server.listen(port, function () {
